555ae8d753b41a35edfbeb868c9803f119ca44d7
Ryo <kanadajinlee@yahoo.ca>
2017-07-13 02:31:34 -0500

    Reverted too earlier mongoose version because of a bug that the mongoose team needs to fix in their latest version

--- scraper/index.js
+++ scraper/index.js
@@ -7,15 +7,12 @@ const CronJob       = require('cron').CronJob;
 const moment        = require("moment");
 
 const rightNow = moment().format('MMMM Do YYYY, h:mm:ss a'); //"May 22nd 2017, 5:38:04 pm"
-console.log("Date & time", rightNow);
+console.log("Date & time:", rightNow);
 
-const everyMorning = new CronJob('00 15 7 * * 0-6', () => { // This cronjob will run at 7:15:00(AM) everyday
+const everyMorning = new CronJob('00 15 7 * * 0-6', () => { // This will run at 7:15:00(AM) everyday
   console.log("It's probably 7:15 AM... but the precise moment is", rightNow);
-
-  /***** Put all of the scrapers inside here *****/
   definitionOTD();
   factOTD();
   newsOTD();
   quotationOTD();
-
 }, null, true, 'America/Chicago');
