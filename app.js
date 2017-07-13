c6cc13b043e5c08ca234ffbde5427763f77f0ae9
Ryo <kanadajinlee@yahoo.ca>
2017-07-13 14:07:52 -0500

    Added Node version (in package.json), Added definition route

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
 const express = require("express");
 const app = express();
-const scraper = require("./scraper")
-const Fact    = require("./models/fact");
+const scraper = require("./scraper");
 
-// Next Steps:
-// 1.  Set up mongo
-// 2.  Connect mongo to express - mongoose
-// 2.5 Set up mongoose table structure (possible that created_at and updated_at are automatic)
-// 3.  Insert data from scraper into mongo
-// 4.  Create route that spits out data from mongo
-
+const Definition = require("./models/definition");
+const Fact       = require("./models/fact");
+const News       = require("./models/news");
+const Quotation  = require("./models/quotation");
 
 app.use(express.static("public"));
 
@@ -23,6 +19,13 @@ app.get("/quotes", (req, res) => {
   // search for most recently created_at
   res.json(["Heyyyyy", 'more stufff']);
 });
+
+app.get("/definition", (req, res) => {
+  Definition.findOne({}, {}, { sort: { 'created_at' : -1 } }, (err, word) => {
+    console.log("Newest word", word)
+    return res.json(word);
+  });
+})
 
 app.get("/fact", (req, res) => {
   Fact.findOne({}, {}, { sort: { 'created_at' : -1 } }, (err, newestFact) => {
