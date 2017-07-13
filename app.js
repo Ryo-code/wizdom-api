72d28003248696ae68473499a0e64ce029db4444
Ryo <kanadajinlee@yahoo.ca>
2017-07-13 16:36:48 -0500

    Added ROOT route

--- app.js
+++ app.js
@@ -7,23 +7,14 @@ const Fact       = require("./models/fact");
 const News       = require("./models/news");
 const Quotation  = require("./models/quotation");
 
-app.use(express.static("public"));
-
 app.get("/", (req, res) => {
-  res.send("Yo! Whassup");
-});
-
-app.get("/quotes", (req, res) => {
-  // access quotes from database
-  // return array of quotes, or 1
-  // search for most recently created_at
-  res.json(["Heyyyyy", 'more stufff']);
+  res.send("This is the Wizdom API");
 });
 
 app.get("/definition", (req, res) => {
-  Definition.findOne({}, {}, { sort: { 'created_at' : -1 } }, (err, word) => {
-    console.log("Newest word", word)
-    return res.json(word);
+  Definition.findOne({}, {}, { sort: { 'created_at' : -1 } }, (err, newestWord) => {
+    console.log("Newest word", newestWord)
+    return res.json(newestWord);
   });
 })
 
@@ -31,6 +22,20 @@ app.get("/fact", (req, res) => {
   Fact.findOne({}, {}, { sort: { 'created_at' : -1 } }, (err, newestFact) => {
     console.log("Newest fact", newestFact)
     return res.json(newestFact);
+  });
+})
+
+app.get("/news", (req, res) => {
+  News.findOne({}, {}, { sort: { 'created_at' : -1 } }, (err, topNews) => {
+    console.log("Top news", topNews)
+    return res.json(topNews);
+  });
+})
+
+app.get("/quotation", (req, res) => {
+  Quotation.findOne({}, {}, { sort: { 'created_at' : -1 } }, (err, newestQuote) => {
+    console.log("Newest quote", newestQuote)
+    return res.json(newestQuote);
   });
 })
 
