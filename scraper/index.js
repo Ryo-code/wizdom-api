a6c6af6656fefe89f2c2fc5f9516efb18f29ec45
Ryo <kanadajinlee@yahoo.ca>
2017-07-14 13:54:49 -0500

    Testing Cronjob

--- scraper/index.js
+++ scraper/index.js
@@ -6,9 +6,14 @@ const quotationOTD  = require("./quotation");
 const CronJob       = require('cron').CronJob;
 const moment        = require("moment");
 
-const everyMorning = new CronJob('00 15 7 * * 0-6', () => { // This will run at 7:15:00(AM) everyday
+//Cronjob arguments: Seconds(0-59) Minutes(0-59) Hours(0-23) Day_Of_Month(1-31) Months(0-11) Day_Of_Week(0-6)
+
+// const everyMorning = new CronJob('00 15 7 * * 0-6', () => { // This will run at 7:15:00(AM) everyday
+new CronJob('* 5 * * * *', () => { //for testing purposes
   const rightNow = moment().format('MMMM Do YYYY, h:mm:ss a'); //"May 22nd 2017, 5:38:04 pm"
+  console.log("=================================================================");
   console.log("It's probably 7:15 AM... but the precise moment is", rightNow);
+  console.log("=================================================================");
   definitionOTD();
   factOTD();
   newsOTD();
