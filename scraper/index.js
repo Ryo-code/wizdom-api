a1d9b04ec459567ae2138a4b892fe6fc64567b17
Ryo <kanadajinlee@yahoo.ca>
2017-07-14 14:41:34 -0500

    Making sure the scraper works on Heroku (with Cron)

--- scraper/index.js
+++ scraper/index.js
@@ -6,13 +6,20 @@ const quotationOTD  = require("./quotation");
 const CronJob       = require('cron').CronJob;
 const moment        = require("moment");
 
-//Cronjob arguments: Seconds(0-59) Minutes(0-59) Hours(0-23) Day_Of_Month(1-31) Months(0-11) Day_Of_Week(0-6)
 
-// const everyMorning = new CronJob('00 15 7 * * 0-6', () => { // This will run at 7:15:00(AM) everyday
-new CronJob('* 5 * * * *', () => { //for testing purposes
+//Cronjob arguments: Seconds(0-59) Minutes(0-59) Hours(0-23) Day_Of_Month(1-31) Months(0-11) Day_Of_Week(0-6)
+//"10" doesn't mean "run every 10 seconds," it means "run on the 10th second of every minute"
+//Therefore: ('0 * * * * *') = hourly; ('0 0 * * * *') = daily
+
+// new CronJob('* * * * * *', () => { //for testing purposes
+//   console.log(rightNow)
+// }, null, true, 'America/Chicago');
+
+// const everyMorning = new CronJob('* * 7 * * *', () => { // This will run at 7:15:00(AM) everyday
+new CronJob('15 * * * * *', () => { //for testing purposes
   const rightNow = moment().format('MMMM Do YYYY, h:mm:ss a'); //"May 22nd 2017, 5:38:04 pm"
   console.log("=================================================================");
-  console.log("It's probably 7:15 AM... but the precise moment is", rightNow);
+  console.log("Inputting data –", rightNow);
   console.log("=================================================================");
   definitionOTD();
   factOTD();
