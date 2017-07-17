84336885a3e2b59806f5fd2432cac7f8d080a625
Ryo <kanadajinlee@yahoo.ca>
2017-07-16 22:08:19 -0500

    Added a new cronjob to make a console log every hour

--- scraper/index.js
+++ scraper/index.js
@@ -6,6 +6,11 @@ const wordOTD      = require("./word");
 const CronJob      = require('cron').CronJob;
 const moment       = require("moment");
 
+const hourly message new CronJob('0 0 * * * *', () => {
+  const rightNow = moment().format('MMMM Do YYYY, h:mm:ss a');
+  console.log("DING! It's", rightNow);
+}, null, true, 'America/Chicago');
+
 //Cronjob arguments: Seconds(0-59) Minutes(0-59) Hours(0-23) Day_Of_Month(1-31) Months(0-11) Day_Of_Week(0-6)
 const everyMorning = new CronJob('0 15 7 * * *', () => { // This will run at 7:15:00(AM) everyday
   const rightNow = moment().format('MMMM Do YYYY, h:mm:ss a'); //"May 22nd 2017, 5:38:04 pm"
