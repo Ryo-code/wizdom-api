d1a41221ba98f0560520fe59bba033c377c8431a
Ryo <kanadajinlee@yahoo.ca>
2017-08-13 10:03:04 -0400

    Changed procfile and added timestamp (gong) to heroku scheduler

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 web: node app.js
 worker: node scraper/index.js
+timestamp: node gong.js
--- gong.js
+++ gong.js
@@ -1,13 +1,13 @@
-#!/usr/bin/env node
-
-//TODO: If this doesn't work, take out the shebang at the top
+const moment       = require("moment");
 
 function logMessage() {
+  const rightNow = moment().format('MMMM Do YYYY, h:mm:ss a'); //"August 22nd 2017, 5:38:04 pm"
   console.log('-=-  -=-  -=-  -=-  -=-  -=-  -=-  -=-  -=-  -=-  ');
   console.log("");
   console.log('GONG!');
   console.log('This console log comes from the herokuCron.js file in the ROOT folder');
   console.log("");
+  console.log("According to moment.js, it is:", rightNow )
   console.log('-=-  -=-  -=-  -=-  -=-  -=-  -=-  -=-  -=-  -=-  ');
 }
 
