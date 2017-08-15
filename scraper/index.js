57993127e4fe6574e5ab89d00527f7d367f69e1c
Ryo <kanadajinlee@yahoo.ca>
2017-08-15 16:19:07 -0400

    returning scrapers

--- scraper/index.js
+++ scraper/index.js
@@ -7,20 +7,20 @@ const CronJob      = require('cron').CronJob;
 const moment       = require("moment");
 
 const logMessage = new CronJob('0 0,15,30,45 * * * *', () => {
+  console.log("DING! It's", moment().format('h:mm a (MMMM Do)') );
 }, null, true, 'America/Los_Angeles');
 
 //Cronjob arguments: Seconds(0-59) Minutes(0-59) Hours(0-23) Day_Of_Month(1-31) Months(0-11) Day_Of_Week(0-6)
 const everyMorning = new CronJob('0 16 7 * * *', () => {
   const rightNow = moment().format('MMMM Do YYYY, h:mm:ss a'); //"August 22nd 2017, 5:38:04 pm"
 
+  console.log("=================================================================");
+  console.log("This message is brought to you by... scraper/index.js")
+  console.log("Scraping data & entering into DB –", rightNow);
+  console.log("=================================================================");
 
-  quotationOTD();
+  factOTD();
   newsOTD();
+  quotationOTD();
   wordOTD();
 }, null, true, 'America/Los_Angeles');
-  console.log("=================================================================");
-  console.log("DING! It's", moment().format('h:mm a (MMMM Do)') );
-  // console.log("This message is brought to you by... scraper/index.js")
-  console.log("Scraping FACT data & entering into DB –", rightNow);
-  console.log("=================================================================");
-  factOTD();
